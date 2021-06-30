679a90e28dcdb05aad4b80b498c98d22ac5c319f
pmtruong24 <55790555+pmtruong24@users.noreply.github.com>
2021-06-30 22:19:35 +0700

    Xem Cau Hoi Da Duyet

---
+++ FeedbackForITStudents/Areas/Admin/Controllers/XemCauHoiController.cs
@@ -0,0 +1,24 @@
+﻿using FeedbackForITStudents.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebCanteen.Areas.Admin.Middleware;
+
+namespace FeedbackForITStudents.Areas.Admin.Controllers
+{
+    [LoginVerification]
+    public class XemCauHoiController : Controller
+    {
+        SEP24Team12Entities model = new SEP24Team12Entities();
+
+        // GET: Admin/XemCauHoi
+        public ActionResult Index()
+        {
+            var cauHoi = model.CAUHOIDADUYETs.OrderByDescending(c => c.pin == true);
+
+            return View(cauHoi);
+        }
+    }
+}
